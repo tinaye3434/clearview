1c0db47a0ee6943fda29af3ef6547bcad4492b0a
Tinaye Katemba <64467034+tinaye3434@users.noreply.github.com>
2025-04-08 07:40:13 +0200

    donor side

--- app/Livewire/Donors/FundingRequest/Index.php
+++ app/Livewire/Donors/FundingRequest/Index.php
@@ -2,8 +2,10 @@
 
 namespace App\Livewire\Donors\FundingRequest;
 
+use App\Models\FundingRequest;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 #[Layout('components.layouts.user')]
 class Index extends Component
@@ -11,6 +13,8 @@ class Index extends Component
 
     public function render()
     {
-        return view('livewire.donors.funding-request.index');
+        return view('livewire.donors.funding-request.index', [
+            'fundingRequests' => FundingRequest::where('is_funded', 0)->paginate(3),
+        ]);
     }
 }
